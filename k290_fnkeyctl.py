af887ab678a988a0d27898d5b1a8de9ba06e8689
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2015-06-22 12:06:07 +0200

    Replace tab with spaces

--- k290_fnkeyctl.py
+++ k290_fnkeyctl.py
@@ -16,22 +16,22 @@ USB_VENDOR_LOGITECH = 0x046d
 USB_PRODUCT_K290 = 0xc31f
 
 def main(argv):
-	context = usb1.USBContext()
-	device = context.openByVendorIDAndProductID(USB_VENDOR_LOGITECH,USB_PRODUCT_K290)
-	if not device:
-		print('Logitech K290 not found on this system')
-		return 2
-	
-	reset = 0 if '-r' in argv or '--reset' in argv else 1
-		
-	err = device.controlWrite(0x40,2,0x001a,reset,b'',1000)
-	if not err:
-		behaviour = "regular" if reset else "default"
-		print('Successfully set function keys to {0} behaviour'.format(behaviour))
-		return 0
-	else:
-		print('Something went wrong when sending the command: {0}'.format(err))
-		return 1
-	
+    context = usb1.USBContext()
+    device = context.openByVendorIDAndProductID(USB_VENDOR_LOGITECH,USB_PRODUCT_K290)
+    if not device:
+        print('Logitech K290 not found on this system')
+        return 2
+    
+    reset = 0 if '-r' in argv or '--reset' in argv else 1
+        
+    err = device.controlWrite(0x40,2,0x001a,reset,b'',1000)
+    if not err:
+        behaviour = "regular" if reset else "default"
+        print('Successfully set function keys to {0} behaviour'.format(behaviour))
+        return 0
+    else:
+        print('Something went wrong when sending the command: {0}'.format(err))
+        return 1
+    
 if __name__ == '__main__':
-	sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
